816e6ecaaa3eab99dffe766c7f7c3e7be4b747bb
jo <jojoo9279@gmail.com>
2023-05-03 14:42:47 +0900

    [문제 정리] 평균 구하기 짝수 홀수 자리수 더하기

--- src/main/java/org/example/solution1.java
+++ src/main/java/org/example/solution1.java
@@ -1,18 +1,15 @@
 package org.example;
 
-public class solution1 {
+class solution1 {
     public double solution(int[] arr) {
         double answer = 0;
-        double tot =0;
-        for(int i: arr){
-            tot += i;
+
+        for(int i = 0; i < arr.length ; i++){
+            answer += arr[i];
         }
-        answer = tot/(arr.length);
+        answer = answer/arr.length;
+
         return answer;
     }
 }
-//
-
-// 주석에 쓸 내용 : 자신이 이 문제를 어떻게 풀었는지 쓰기, 어려워서 못 풀었으면 어떤 부분이 어려웠는지 남기기.
-// solution1에 첫문제 풀고 다음날 부터 솔루션2,3,4.. 클래스 만들어서 풀고 커밋하기
-// gpt 사용금지.
+//평균 구하기
