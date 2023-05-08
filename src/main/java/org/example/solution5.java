616d83e7d58ce6542696aa3d8c1e9f6232971743
YoungDong99 <114642786+YoungDong99@users.noreply.github.com>
2023-05-08 09:07:31 +0900

    [나머지가 1이되는 수 찾기]
    주석 추가

--- src/main/java/org/example/solution5.java
+++ src/main/java/org/example/solution5.java
@@ -1,10 +1,14 @@
 package org.example;
 
 public class solution5 {
+    //나머지가 1이 되는 수 찾기
     public int solution(int n) {
+        //나머지가 1이 되려면 1보다 커야하므로 2부터 시작
         for(int i = 2; i < n; i++) {
             if(n % i == 1) return i;
+            //수를 찾으면 종료
         }
         return 0;
     }
+
 }
